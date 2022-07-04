f2c4d28aeb34f38d0e38fba8a0602b9a2ed2a54e
Λ L F I Λ N 男 <98892565+flux10n@users.noreply.github.com>
2022-07-04 10:35:47 +0700

    Update indo.php

--- indo.php
+++ indo.php
@@ -1,5 +1,5 @@
 <?php
-//hargai author dengan cara menggunakan script ini dan tidak merename nama pembuat dan mereuploadnya!!!
+//hargai author dengan cara menggunakan script ini dan tidak merename nama pembuat atau mereuploadnya!!!
 session_start();
 @error_reporting(0);
 @set_time_limit(0);
